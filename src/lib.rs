0c0ef5e855127cced8b43e4424db485195578ec6
Peter Stuart <peter@peterstuart.org>
2021-11-14 08:25:34 -0500

    Add package metadata (#5)
    * Update package metadata
    
    * Add lib-level docs and README.md
    
    * Enfore up to date README in CI

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+//! An interpolator for analog-to-digital converters.
+//!
+//! Convert voltage readings from an ADC into meaningful values using
+//! linear interpolation.
+//!
+//! # Examples
+//!
+//! ```
+//! use adc_interpolator::{AdcInterpolator, pair};
+//! # use embedded_hal_mock::{
+//! #     adc::{Mock, MockChan0, Transaction},
+//! #     common::Generic,
+//! #     MockError,
+//! # };
+//! #
+//! # let pin = MockChan0 {};
+//! # let expectations: [Transaction<u16>; 1] = [Transaction::read(0, 614)];
+//! # let mut adc = Mock::new(&expectations);
+//! # let pin = MockChan0 {};
+//!
+//! let mut interpolator = AdcInterpolator::new(
+//!     pin,
+//!     [
+//!         // 1000 mV maximum voltage, 12-bit precision
+//!         pair(1000, 12, 100, 40), // 100 mV -> 40
+//!         pair(1000, 12, 200, 30), // 200 mV -> 30
+//!         pair(1000, 12, 300, 10), // 300 mV -> 10
+//!     ],
+//! );
+//!
+//! // With voltage at 150 mV, the value is 35
+//! assert_eq!(interpolator.read(&mut adc), Ok(Some(35)));
+//! ```
+
 #![cfg_attr(not(test), no_std)]
 
 mod interpolate;
@@ -123,7 +157,7 @@ impl<Pin, Word, const LENGTH: usize> AdcInterpolator<Pin, Word, LENGTH> {
     ///     ],
     /// );
     ///
-    /// // With voltage at 0.15V, the value is 35
+    /// // With voltage at 150 mV, the value is 35
     /// assert_eq!(interpolator.read(&mut adc), Ok(Some(35)));
     /// ```
     pub fn read<Adc, ADC>(
